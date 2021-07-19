a019ade9e35e7dc78c6ab61597d17c78ef3ba4dd
Dinara.Aleskarova <aleskarova@gmail.com>
2021-07-20 01:35:37 +0400

    feat: solve reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
-}
+  let result = 0;
+  let current = Math.abs(n);
+  do {
+    const remainder = current % 10;
+    result = result * 10 + remainder;
+    current = Math.floor(current / 10);
+  } while (current != 0);
+
+  return result;
+};
